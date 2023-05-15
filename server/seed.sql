458321e39c3007ef62689dcd5c2a7850e66f34a9
TDow520 <dow.tailon@gmail.com>
2023-05-15 14:18:03 -0500

    finalized dummy database data

--- server/seed.sql
+++ server/seed.sql
@@ -1,1 +1,7 @@
-INSERT INTO Tracks (id, name, duration, plays, album, artist) VALUES ("3cHyrEgdyYRjgJKSOiOtcS", "Timber (feat. Ke$ha)", 204160, 83, "Global Warming: Meltdown (Deluxe Version)", 'Pit Bull');
+INSERT INTO Tracks (id, name, duration, plays, album, artist) VALUES ('3cHyrEgdyYRjgJKSOiOtcS', 'Timber (feat. Ke$ha)', 204160, 83, 'Global Warming: Meltdown (Deluxe Version)', 'Pit Bull');
+
+INSERT INTO Albums (id, album_name, year_made, song_amount, total_duration, artist, photo) VALUES ('7I4Vx91kradxSJNwg1ycbq', 'Global Warming: Meltdown (Deluxe Version)', 2012, 16, 204160, 'Pit Bull', 'https://i.scdn.co/image/ab67616d0000b273f575130838bc98c4f2b17312');
+
+INSERT INTO Artists (id, artist_name, followers, image) VALUES ('0TnOYISbd1XYRBk9myaseg', 'Pit Bull', 9986097, 'https://i.scdn.co/image/ab6761610000e5ebfc9d2abc85b6f4bef77f80ea');
+
+
