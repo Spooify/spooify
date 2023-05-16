8bfa4c2a098017bfcef04a7288940c0f76f371ee
TDow520 <dow.tailon@gmail.com>
2023-05-16 16:28:56 -0500

    finalized side bar appearance. need to get small functionality in later additions.

---
+++ client/src/sidebar/Sidebar.jsx
@@ -0,0 +1,55 @@
+import "./Sidebar.css"
+
+
+
+// import spooify_white from "../sidebar/images/spooify_white.png"
+
+
+function Sidebar(){
+
+    const searchimgURL = new URL("./images/search_icon.png", import.meta.url);
+    const homeimgURL = new URL("./images/home_icon.png", import.meta.url);
+    return(
+        <div className="sidebar">
+            <div className="sidebarItem">
+                {/* <div className="spooify">
+                    <img src={spooify_white} alt="" /> 
+                    Spooify
+                </div> */}
+                <div className="home">
+                    <img src={homeimgURL} alt="home" className="homeimg"/>
+                    Home
+                    {/* <img src="client/src/assets/react.svg" alt="" /> */}
+                </div>
+                <div className="search">
+                    <img src={searchimgURL} alt="search" className="searchimg"/>
+                    Search
+                </div>
+
+            </div>
+            <div className="playlist">
+                <div className="userList">
+                    <div className="libLine"></div>
+                    <div className="libLine"></div>
+                    <div className="libIcon"></div>
+                    <p className="playlistTitle">
+                        Your Library
+                    </p>
+                </div>
+                <div className="buttons">
+                    <button>
+                        Playlists
+                    </button>
+                    <button>
+                        Artists
+                    </button>
+                    <button>
+                        Albums
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
