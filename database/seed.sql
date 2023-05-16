1e6c3ccccc68c7bc6e478bbb42d262277d025d7a
jrm0527 <geeziegames@gmail.com>
2023-05-16 15:54:41 -0400

    Added Discography/Album Components

--- database/seed.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO Artists (id, artist_name, followers, image) VALUES ('0TnOYISbd1XYRBk9myaseg', 'Pit Bull', 9986097, 'https://i.scdn.co/image/ab6761610000e5ebfc9d2abc85b6f4bef77f80ea');
-
-INSERT INTO Albums (id, album_name, year_made, song_amount, total_duration, photo) VALUES ('7I4Vx91kradxSJNwg1ycbq', 'Global Warming: Meltdown (Deluxe Version)', 2012, 16, 204160, 'https://i.scdn.co/image/ab67616d0000b273f575130838bc98c4f2b17312');
-
-INSERT INTO Tracks (id, name, duration, album, artist) VALUES ('3cHyrEgdyYRjgJKSOiOtcS', 'Timber (feat. Ke$ha)', 204160, '7I4Vx91kradxSJNwg1ycbq', '0TnOYISbd1XYRBk9myaseg');
-
