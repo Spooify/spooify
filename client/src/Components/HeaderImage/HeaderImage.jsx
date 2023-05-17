87381a901c58a3da400256a0b839e44068433afa
tpeterec <alex1996p3@gmail.com>
2023-05-17 13:51:54 -0800

    Added HeaderImage component

---
+++ client/src/Components/HeaderImage/HeaderImage.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from "react";
+import "./HeaderImage.css";
+
+useEffect(() => {
+    const url = `http://localhost:4000/api/albums/`
+});
+const HeaderImage = (props) => {
+  return (<div>Hello</div>
+
+  );
+}
+
+export default HeaderImage;
